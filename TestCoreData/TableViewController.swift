97196279d0027b84c361ae220cc346bb564f61f3
Топорков Максим <toporkovmaksim7@gmail.com>
2020-05-21 18:13:32 +0700

    fix

--- TestCoreData/TableViewController.swift
+++ TestCoreData/TableViewController.swift
@@ -11,24 +11,25 @@ import CoreData
 
 class TableViewController: UITableViewController {
     
+    //массив сущностей
     lazy var tasks: [Task] = []
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+        //получаем контекст
         let context = getContext()
+        //запрос на получение сущности Task
         let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
         do {
+            //присваеваем массиву tasks, полученный массив данных из fetchRequest
             try tasks = context.fetch(fetchRequest)
         } catch let error as NSError {
             print(error.localizedDescription)
         }
-        
+    }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
 
     @IBAction func addButton(_ sender: Any) {
@@ -50,6 +51,7 @@ class TableViewController: UITableViewController {
     @IBAction func deleteAction(_ sender: Any) {
         let context = getContext()
         let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        //создаем массив объектов, которые содержатся в сущности Task, с помощью fetchRequest
         if let objects = try? context.fetch(fetchRequest) {
             for object in objects {
                 context.delete(object)
@@ -57,6 +59,7 @@ class TableViewController: UITableViewController {
         } else {
             return
         }
+        //сохраняем контекст после удаления объектов сущности
         do {
             try context.save()
         } catch let error as NSError {
@@ -65,11 +68,16 @@ class TableViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
+    //сохраняет в базу полученную строку
     private func saveTask(withTitle title: String) {
         let context = getContext()
+        //создаем сущность для сущности Task, в контексте
         guard let entity = NSEntityDescription.entity(forEntityName: "Task", in: context) else { return }
+        //создаем объект равный сущности Task
         let taskObjekt = Task(entity: entity, insertInto: context)
+        //присваеваем тайтлу сущности Task пришедшую в параметры строку
         taskObjekt.title = title
+        //сохраняем контекст в базе и добавляем taskObjekt в массив tasks
         do {
             try context.save()
             tasks.insert(taskObjekt, at: 0)
@@ -103,51 +111,4 @@ class TableViewController: UITableViewController {
 
         return cell
     }
-   
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
